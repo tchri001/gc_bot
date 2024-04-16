38f75dce920f202168765a8d44ebc86fbc7a976f
tchristo <tim.christophe@hotmail.com>
2024-04-16 14:42:42 +0800

    Find icon on screen

--- main.py
+++ main.py
@@ -6,3 +6,5 @@ import autopy
 if __name__ == '__main__':
     autopy.alert.alert("Waiting...")
     pyautogui.screenshot('images/screenshots/main_page.png')
+    to_battle_loc = pyautogui.locateOnScreen('images/to_battle.png')
+    print(to_battle_loc)
