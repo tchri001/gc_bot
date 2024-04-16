d756c9e587ad59925dfbf661c1009bfac49e0f84
tchristo <tim.christophe@hotmail.com>
2024-04-16 23:21:33 +0800

    Open BlueStacks and start battle

--- main.py
+++ main.py
@@ -2,9 +2,38 @@ import cv2
 import numpy as np
 import pyautogui
 import autopy
+import time
 
+def fast_click(x, y):
+    pyautogui.moveTo(x, y, 0)
+    pyautogui.click()
+
+def slow_click(x, y):
+    pyautogui.moveTo(x, y, 0.1)
+    pyautogui.mouseDown()
+    time.sleep(0.1)
+    pyautogui.mouseUp()
+
+def find_and_click(image, fast: bool = False):
+    click_method = None
+    if fast:
+        click_method = fast_click
+    else:
+        slow_click
+    print('images/'+image+'.png')
+    x, y = pyautogui.locateCenterOnScreen('images/'+image+'.png', confidence=0.8)
+    click_method
+    
 if __name__ == '__main__':
-    autopy.alert.alert("Waiting...")
-    pyautogui.screenshot('images/screenshots/main_page.png')
-    to_battle_loc = pyautogui.locateOnScreen('images/to_battle.png')
-    print(to_battle_loc)
+    #autopy.alert.alert("Waiting...")
+    bs_x, bs_y = pyautogui.locateCenterOnScreen('images/open_bluestacks.png', confidence=0.8)
+    pyautogui.moveTo(bs_x, bs_y, 0.4)
+    pyautogui.mouseDown()
+    time.sleep(0.2)
+    pyautogui.mouseUp()    
+
+    bt_x, bt_y = pyautogui.locateCenterOnScreen('images/to_battle.png', confidence=0.8)
+    pyautogui.moveTo(bt_x, bt_y, 1)
+    pyautogui.mouseDown()
+    time.sleep(0.2)
+    pyautogui.mouseUp()
