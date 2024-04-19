bbbaa08220348ff85603bc60fae6a1b41a6530fb
tchristo <tim.christophe@hotmail.com>
2024-04-19 18:04:43 +0800

    Moving activation

--- main.py
+++ main.py
@@ -16,12 +16,12 @@ def enter_game():
     find_and_click('to_battle')
     time.sleep(0.2)
     pyautogui.press('esc')
+    run_game()
 
 # Spam abilities whilst in game
 def run_game():
     global game_status
     
-    enter_game()
     while game_status == 1:
         try:
             find_and_click("activate", user_side=True)
@@ -37,6 +37,7 @@ def check_game_status():
             pyautogui.locateOnScreen(f'images/'+device+'/settings.png')
             game_status = 0
             logger.info('Settings found, status = home screen')
+            #enter_game()
         except pyautogui.ImageNotFoundException:
             game_status = 1
             logger.info('Settings not found, status = in game')
@@ -53,13 +54,13 @@ if __name__ == '__main__':
     
     #Setup monitor tread as a daemon so program dies when main threads finished
     game_status_monitor = threading.Thread(target=check_game_status, args=(), daemon=True)
-    gameplay_loop = threading.Thread(target=run_game, args=())
+    #gameplay_loop = threading.Thread(target=run_game, args=())
 
     # TODO: Check this out for window handling instead of relying on game icon: https://stackoverflow.com/questions/43785927/python-pyautogui-window-handle
     logger.info('Opening Emulator taskbar icon')
     find_and_click("open_bluestacks")
-    time.sleep(0.3)
+    time.sleep(0.5)
 
     logger.info('Starting program...')
     game_status_monitor.start()
-    gameplay_loop.start()
+    enter_game()
